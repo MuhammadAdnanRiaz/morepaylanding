4b42f176dd480f1282608d617b6d0779c3b72b24
Muhammad Adnan Riaz <andanriaz511@gmail.com>
2024-10-06 12:20:48 +0500

    fixed console error and added available in country section

--- app/components/AvailableCountrySection.tsx
+++ app/components/AvailableCountrySection.tsx
@@ -2,22 +2,30 @@ import { countriesList } from "../utils/countries";
 
 export default function AvailableinCountrySection() {
   return (
-    <section className="w-full bg-gray-100 py-8">
-      <div className="max-w-7xl mx-auto">
-        <span className="text-6xl font-semibold">
-          Send money to over 20+ Countries across the world
-        </span>
-        <div className="grid grid-cols-5 gap-8">
-          {countriesList.slice(0, 21).map((country) => {
-            return (
-              <div key={country.code}>
-                <p className="text-6xl">{country.emoji}</p>
-                <p className="">{country.name}</p>
-              </div>
-            );
-          })}{" "}
-        </div>
+    <section className="bg-gray-100 w-full">
+      <div className="max-w-7xl w-full px-5 py-24 mx-auto">
+        <div className="flex flex-wrap -m-4">
+          <div className="lg:mb-0 mb-6 p-4">
+            <div className="flex items-center justify-center w-full">
+            <span className="text-4xl text-center sm:text-6xl font-bold">
+              Send money to over 20+ Countries across the world
+            </span>
+            </div>
+           
+            <div className="grid grid-cols-2 sm:grid-cols-5 text-center sm:text-left gap-8 pt-14">
+              {countriesList.slice(0, 21).map((country) => {
+                return (
+                  <div key={country.code}>
+                    <p className="text-5xl">{country.emoji}</p>
+                    <p className="">{country.name}</p>
+                  </div>
+                );
+              })}{" "}
+            </div>
+          </div>
+        </div>  
       </div>
+      
     </section>
   );
 }
--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -104,9 +104,9 @@ export default function Footer() {
             <a className="text-gray-500">
               <svg
                 fill="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 className="w-5 h-5"
                 viewBox="0 0 24 24"
               >
@@ -116,9 +116,9 @@ export default function Footer() {
             <a className="ml-3 text-gray-500">
               <svg
                 fill="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 className="w-5 h-5"
                 viewBox="0 0 24 24"
               >
@@ -129,9 +129,9 @@ export default function Footer() {
               <svg
                 fill="none"
                 stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 className="w-5 h-5"
                 viewBox="0 0 24 24"
               >
@@ -143,9 +143,9 @@ export default function Footer() {
               <svg
                 fill="currentColor"
                 stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="0"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="0"
                 className="w-5 h-5"
                 viewBox="0 0 24 24"
               >
