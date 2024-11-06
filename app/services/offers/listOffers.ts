e42fffa8d2de38f06042669cdb6c2b6d51deec90
Muhammad Adnan Riaz <andanriaz511@gmail.com>
2024-11-06 18:09:24 +0500

    WIP:backend services

---
+++ app/services/offers/listOffers.ts
@@ -0,0 +1,15 @@
+// services/authService.ts
+import { toast } from 'sonner';
+import apiClient from '../apiClient';
+import { Offer } from './types'
+
+export const listOffersService = async (): Promise<Offer[]> => {
+    try {
+        const response = await apiClient.get<Offer[]>('/exchange/offer/get/all/');
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching roles:", error);
+        toast('Something went wrong during listing offers')
+        throw error; // This allows error handling at the component level
+    }
+}
